4d36523ea3fd2ded38d7b5b4b5d16c1508f213ce
1043443209@qq.com <nkily6574.mx>
2020-08-09 21:33:40 +0800

    各种提交

--- src/medium/NumSquares.java
+++ src/medium/NumSquares.java
@@ -15,8 +15,19 @@ public class NumSquares {
         // 25+16+1+1  16+16+9+3 36+4+1+1+1  25+9+9
         System.out.println(solution.numSquares(43));
     }
-
+    // 动态规划
     public int numSquares(int n) {
+        int[] arr = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            arr[i] = i;
+            for (int j = 0; i - j * j >= 0; j++) {
+                arr[i] = Math.min(arr[i], arr[i - j * j]+1);
+            }
+        }
+        return arr[n];
+    }
+    // 递归
+    public int numSquares1(int n) {
         int count = 0;
         int max = (int) Math.sqrt(n);
         for (int i = max; i > 0; i--) {
