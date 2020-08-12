f11e5fe1773e3378861f913f576c21914ee3d764
1043443209@qq.com <nkily6574.mx>
2020-08-12 21:22:19 +0800

    各种提交

---
+++ src/medium/FindBottomLeftValue.java
@@ -0,0 +1,46 @@
+package medium;
+
+import base.TreeNode;
+
+/**
+ * 513. 找树左下角的值
+ */
+public class FindBottomLeftValue {
+
+    private int maxLength = 0;
+
+    private int maxValue = 0;
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node2 = new TreeNode(20);
+        TreeNode node3 = new TreeNode(15);
+        TreeNode node4 = new TreeNode(7);
+        root.left = node1;
+        root.right = node2;
+        node2.left = node3;
+        node2.right = node4;
+
+        FindBottomLeftValue solution = new FindBottomLeftValue();
+        System.out.println(solution.findBottomLeftValue(root));
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        search(0, root);
+        return maxValue;
+    }
+
+    private void search(int length, TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+        length++;
+        if (length > maxLength) {
+            maxValue = treeNode.val;
+            maxLength = length;
+        }
+        search(length, treeNode.left);
+        search(length, treeNode.right);
+    }
+}
