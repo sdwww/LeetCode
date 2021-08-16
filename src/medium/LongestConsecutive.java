be9f69ae451acddcd5bebad4984776ddf751964c
wangweiweibj01 <wangweiweibj01@yuanfudao.com>
2021-08-16 15:01:41 +0800

    ADD: 1.各种提交

--- src/medium/LongestConsecutive.java
+++ src/medium/LongestConsecutive.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 128. 最长连续序列
  *
@@ -9,11 +12,30 @@ public class LongestConsecutive {
 
     public static void main(String[] args) {
         LongestConsecutive longestConsecutive = new LongestConsecutive();
-        int[] nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+//        int[] nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        int[] nums = {4, 0, -4, -2, 2, 5, 2, 0, -8, -8, -8, -8, -1, 7, 4, 5, 5, -4, 6, 6, -3};
         System.out.println(longestConsecutive.longestConsecutive(nums));
     }
 
     public int longestConsecutive(int[] nums) {
-        return 0;
+        int longestConsecutive = 0;
+        Set<Integer> set = new HashSet<>();
+        for (Integer num : nums) {
+            set.add(num);
+        }
+
+        for (Integer item : set) {
+            if (set.contains(item - 1)) {
+                continue;
+            }
+            int currentItem = item;
+            int currentConsecutive = 1;
+            while (set.contains(currentItem + 1)) {
+                currentConsecutive++;
+                currentItem++;
+            }
+            longestConsecutive = Math.max(currentConsecutive, longestConsecutive);
+        }
+        return longestConsecutive;
     }
 }
