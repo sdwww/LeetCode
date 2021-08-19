c1cafb6513a25a6318809b23282ee7c8f58b49be
1043443209@qq.com <nkily6574.mx>
2021-08-19 23:37:10 +0800

    ADD: 1.各种提交

---
+++ src/medium/MyAtoi.java
@@ -0,0 +1,15 @@
+package medium;
+
+/**
+ * 8. 字符串转换整数 (atoi)
+ */
+public class MyAtoi {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int myAtoi(String s) {
+        return 0;
+    }
+}
