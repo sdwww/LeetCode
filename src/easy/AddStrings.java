487566055abbd4e450b43f5e6e6c56450403200a
wangweiweibj01 <wangweiweibj01@yuanfudao.com>
2021-08-23 11:16:08 +0800

    ADD: 1.各种提交

--- src/easy/AddStrings.java
+++ src/easy/AddStrings.java
@@ -9,11 +9,42 @@ public class AddStrings {
 
     public static void main(String[] args) {
         AddStrings addStrings = new AddStrings();
-        String s = addStrings.addStrings("1234", "12345");
+        String s = addStrings.addStrings("1", "99999999");
         System.out.println(s);
     }
 
     public String addStrings(String num1, String num2) {
-        return null;
+        // 倒转字符串
+        StringBuilder reverseNum1 = new StringBuilder(num1).reverse();
+        StringBuilder reverseNum2 = new StringBuilder(num2).reverse();
+        int length = Math.max(num1.length(), num2.length());
+        // 补零对齐
+        if (reverseNum1.length() < length) {
+            for (int i = 0; i < length - num1.length(); i++) {
+                reverseNum1.append("0");
+            }
+        } else {
+            for (int i = 0; i < length - num2.length(); i++) {
+                reverseNum2.append("0");
+            }
+        }
+        boolean addOne = false;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int sum = Integer.parseInt(reverseNum1.substring(i, i + 1)) + Integer.parseInt(reverseNum2.substring(i, i + 1));
+            int temp = sum + (addOne ? 1 : 0);
+            if (temp >= 10) {
+                result.append(temp - 10);
+                addOne = true;
+            } else {
+                result.append(temp);
+                addOne = false;
+            }
+        }
+        // 最后可能向上进1
+        if (addOne) {
+            result.append("1");
+        }
+        return result.reverse().toString();
     }
 }
--- src/medium/FindDiagonalOrder.java
+++ src/medium/FindDiagonalOrder.java
@@ -9,7 +9,7 @@ public class FindDiagonalOrder {
 
     public static void main(String[] args) {
         int[][] nums = new int[][]{
-//                {1, 2, 3},
+                {1, 2, 3},
 //                {4, 5, 6},
                 {7, 8, 9}};
         FindDiagonalOrder solution = new FindDiagonalOrder();
@@ -19,27 +19,29 @@ public class FindDiagonalOrder {
         }
     }
 
+    // 时间复杂度O(m*n)
     public int[] findDiagonalOrder(int[][] mat) {
         int[] result = new int[mat.length * mat[0].length];
         int count = 0;
         // 对角线的横坐标和纵坐标之和为常数i
         for (int i = 0; i < mat.length + mat[0].length - 1; i++) {
-            for (int j = 0; j < mat.length && i - j >= 0; j++) {
+            // 确定初始位置，避免无效循环
+            for (int j = Math.max(0, i - mat[0].length); j < mat.length && i - j >= 0; j++) {
                 if (i % 2 == 0) {
-                    continue;
+                    break;
                 }
                 // 边界控制
-                if (i - j < 0 || i - j >= mat[0].length) {
+                if (i - j >= mat[0].length) {
                     continue;
                 }
                 result[count++] = mat[j][i - j];
             }
-            for (int j = 0; j < mat[0].length; j++) {
+            for (int j = Math.max(0, i - mat.length); j < mat[0].length && i - j >= 0; j++) {
                 if (i % 2 == 1) {
-                    continue;
+                    break;
                 }
                 // 边界空值
-                if (i - j < 0 || i - j >= mat.length) {
+                if (i - j >= mat.length) {
                     continue;
                 }
                 result[count++] = mat[i - j][j];
