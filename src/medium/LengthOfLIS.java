095de85a71e15bdba81533f2e32de31b5214d632
1043443209@qq.com <nkily6574.mx>
2021-08-25 22:38:30 +0800

    ADD: 1.各种提交

---
+++ src/medium/LengthOfLIS.java
@@ -0,0 +1,32 @@
+package medium;
+
+import java.util.Arrays;
+
+/**
+ * 300. 最长递增子序列
+ */
+public class LengthOfLIS {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{10, 9, 2, 5, 3, 7, 101, 18,0};
+        LengthOfLIS lengthOfLIS = new LengthOfLIS();
+        int i = lengthOfLIS.lengthOfLIS(nums);
+        System.out.println(i);
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int result = 0;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    // 以nums[i]结尾的最长递增子序列
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+}
