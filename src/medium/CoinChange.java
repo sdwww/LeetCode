de2c24a0744ce7f03f7ae3a32615a91b2b71afd0
wangweiweibj01 <wangweiweibj01@yuanfudao.com>
2021-08-29 22:47:20 +0800

    ADD: 1.各种提交

--- src/medium/CoinChange.java
+++ src/medium/CoinChange.java
@@ -1,29 +1,42 @@
+/**
+ * @(#)CoinChange.java, 8月 29, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package medium;
 
 /**
- * 518. 零钱兑换 II
+ * 322. 零钱兑换
+ *
+ * @author www
  */
 public class CoinChange {
 
     public static void main(String[] args) {
-        CoinChange solution = new CoinChange();
-        int[] board = {1, 2, 5};
-        System.out.println(solution.change(5, board));
+        CoinChange coinChange = new CoinChange();
+        int[] nums = {1, 2, 5};
+        int i = coinChange.coinChange(nums, 11);
+        System.out.println(i);
     }
 
-
-    public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        dp[0][0] = 1;
-        for (int i = 1; i <= coins.length; i++) {
+    // todo
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp = new int[coins.length][amount+1];
+        for (int i = 1; i < dp[0].length; i++) {
+            if (i % coins[0] == 0) {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < coins.length; i++) {
             for (int j = 0; j <= amount; j++) {
-                if (j >= coins[i - 1]) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                if (j - coins[i] >= 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]];
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return dp[coins.length][amount];
+        return dp[coins.length - 1][amount];
     }
 }
