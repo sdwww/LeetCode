37e6a04c43b6197a2a8ba1608bbee5d5bcccb345
1043443209@qq.com <nkily6574.mx>
2021-09-13 19:51:29 +0800

    ADD: 1.各种提交

--- src/hard/TotalNQueensTwo.java
+++ src/hard/TotalNQueensTwo.java
@@ -7,7 +7,6 @@ public class TotalNQueensTwo {
 
     private int count = 0;
 
-
     public static void main(String[] args) {
         TotalNQueensTwo solution = new TotalNQueensTwo();
         System.out.println(solution.totalNQueens(8));
@@ -21,32 +20,33 @@ public class TotalNQueensTwo {
 
     /**
      *
-     * @param rowNum 当前行数
+     * @param currentRowNum 当前行数
      * @param n 总个数
      * @param arr 维护的数组
      */
-    private void check(int rowNum, int n, int[] arr) {
-        if (rowNum >= n) {
+    private void check(int currentRowNum, int n, int[] arr) {
+        if (currentRowNum >= n) {
             count++;
         }
         for (int i = 0; i < n; i++) {
-            if (judge(rowNum, i, arr)) {
-                arr[rowNum] = i;
-                check(rowNum + 1, n, arr);
+            if (judge(currentRowNum, i, arr)) {
+                // 不需要清除数据，因为后面的遍历会覆盖
+                arr[currentRowNum] = i;
+                check(currentRowNum + 1, n, arr);
             }
         }
     }
 
     /**
-     *
-     * @param rowNum 当前行数
-     * @param value 当前值
+     * 判断某个位置是否可以放置棋子
+     * @param currentRowNum 当前行数
+     * @param currentValue 当前值
      * @param arr 维护的数组
      * @return 是否可行
      */
-    private boolean judge(int rowNum, int value, int[] arr) {
-        for (int i = 0; i < rowNum; i++) {
-            if (arr[i] == value || Math.abs(arr[i] - value) == Math.abs(i - rowNum)) {
+    private boolean judge(int currentRowNum, int currentValue, int[] arr) {
+        for (int i = 0; i < currentRowNum; i++) {
+            if (arr[i] == currentValue || Math.abs(arr[i] - currentValue) == Math.abs(i - currentRowNum)) {
                 return false;
             }
         }
