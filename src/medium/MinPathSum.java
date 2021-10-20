ffd652940cbfd4201b96ae0c191f0055fdb3ec75
1043443209@qq.com <nkily6574.mx>
2021-10-20 18:42:45 +0800

    ADD: 1.各种提交

--- src/medium/MinPathSum.java
+++ src/medium/MinPathSum.java
@@ -11,18 +11,19 @@ public class MinPathSum {
     }
 
     public int minPathSum(int[][] grid) {
-        int[][] sum = new int[grid.length + 1][grid[0].length + 1];
-        for (int i = 1; i < sum.length; i++) {
-            for (int j = 1; j < sum[0].length; j++) {
-                if (i == 1) {
-                    sum[i][j] = sum[i][j - 1] + grid[i - 1][j - 1];
-                } else if (j == 1) {
-                    sum[i][j] = sum[i - 1][j] + grid[i - 1][j - 1];
-                } else {
-                    sum[i][j] = Math.min(sum[i - 1][j], sum[i][j - 1]) + grid[i - 1][j - 1];
-                }
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < dp[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-        return sum[grid.length][grid[0].length];
+        return dp[grid.length - 1][grid[0].length - 1];
     }
 }
