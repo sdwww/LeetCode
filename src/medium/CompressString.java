a2b2d32c0ce94c2b513313d714d24b76907a1edd
1043443209@qq.com <nkily6574.mx>
2021-11-01 22:09:39 +0800

    ADD: 1.各种提交

---
+++ src/medium/CompressString.java
@@ -0,0 +1,44 @@
+package medium;
+
+/**
+ * 443. 压缩字符串
+ */
+public class CompressString {
+
+    public static void main(String[] args) {
+        char[] chars = {'a','a','b','b','c','c','c'};
+        CompressString solution = new CompressString();
+        int compress = solution.compress(chars);
+        System.out.println(compress);
+    }
+
+    public int compress(char[] chars) {
+        char current = chars[0];
+        int count = 0;
+        int result = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (current == chars[i]) {
+                count++;
+            } else {
+                chars[result++] = current;
+                result = fillCount(chars, count, result);
+                current = chars[i];
+                count = 1;
+            }
+        }
+        chars[result++] = current;
+        return fillCount(chars,count,result);
+    }
+
+    // 补全数字部分
+    private int fillCount(char[] chars, int count, int result) {
+        if (count == 1) {
+            return result;
+        }
+        char[] countToChar = String.valueOf(count).toCharArray();
+        for (int j = 0; j < countToChar.length; j++) {
+            chars[result + j] = countToChar[j];
+        }
+        return result + countToChar.length;
+    }
+}
